505d8b08d94bdcdc4d620a0852b3c62b41630f0f
sebastian <yamilsebastiansaez@gmail.com>
2022-09-21 02:58:13 -0300

    javascript

---
+++ JavasCript/javascript.js
@@ -0,0 +1,25 @@
+// sebastian = "chupala"
+// alert(sebastian)
+
+// let numero1 = 29, numero2 = 1 
+// alert(numero1 + numero2)
+
+// prompt ("dime tu nombre");
+
+// let nombre = prompt("dime tu nombre") 
+// alert ("Hola " + nombre + " Chupate una Pija")
+
+
+// calculadora
+
+// let calculo = prompt("indique un numero")
+// let calculo2 = prompt("indique otro numero")
+// alert (calculo + calculo2 + " putito")
+
+
+// metodos
+
+// let lorem = ("sar asa sasadarasasa")
+// console.log(
+//     lorem.includes("sir",)
+// )
